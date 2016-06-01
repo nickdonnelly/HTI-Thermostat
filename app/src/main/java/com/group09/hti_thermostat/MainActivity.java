49efa2f4cb287f511a17e78f18218b0c5f15c573
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-06-01 14:49:16 +0200

    +Volley for HTTP requests. +Framework for parsing the XML. Don't actually know if this works yet because when I went to test it BOTH the real API and the backup one were down...

--- app/src/main/java/com/group09/hti_thermostat/MainActivity.java
+++ app/src/main/java/com/group09/hti_thermostat/MainActivity.java
@@ -1,13 +1,111 @@
 package com.group09.hti_thermostat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // This can be requested to get *all* of the information stored in the web API.
+    public static final String API_BASE_URL = "http://wwwis.win.tue.nl/2id40-ws/9";
+    public static final String API_BACKUP_BASE_URL = "http://pcwin889.win.tue.nl/2id40-ws/9";
+    public static double CurrentTemperature; // These 4 may not be used, not sure yet.
+    public static double DayTemperature;
+    public static double NightTemperature;
+    public static ArrayList WeekProgram;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // This queue makes the request
+        RequestQueue initialQueue = Volley.newRequestQueue(this);
+        // stringrequest holds the initial request data
+        StringRequest strReq = new StringRequest(Request.Method.GET, API_BACKUP_BASE_URL, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String httpResponse){
+                parseEntireDataset(httpResponse);
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError err){
+                //TODO: Make an error popup
+            }
+        });
+        initialQueue.add(strReq); // queue the request to be made.
+    }
+
+    private void parseEntireDataset(String response){
+        //TODO: Parse the XML response, check for error, then change the
+        //      view to reflect whats on the server.
+
+        // First we need an XML parser, android has these built in. We use
+        // a try-catch because this way we don't have to throw exceptions at
+        // the function level.
+        try{
+            XmlPullParserFactory xmlFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = xmlFactory.newPullParser();
+
+            parser.setInput(stringToInputStream(response), null);
+
+            WeekProgram = new ArrayList();
+            // Now we loop until we reach the end of the XML
+            try{
+                while(parser.next() != XmlPullParser.END_TAG){
+                    if(parser.getEventType() != XmlPullParser.START_TAG){
+                        continue;
+                    }
+                    String tagName = parser.getName();
+                    // Send it to ThermostatData if it doesn't have a week program related tag
+                    if(!tagName.equals("week_program") && !tagName.equals("day")){
+                        ThermostatData.readThermostatData(parser, tagName);
+                    }
+                }
+            }catch (IOException e){
+                // TODO: Handle exception
+            }
+
+
+        }catch (XmlPullParserException e){
+            // TODO: Handle PullParserException
+            Log.e("Error: ", e.toString()); // log it
+        }
+        Log.i("XMLPARSE", "Done parsing!");
+        new AlertDialog.Builder(this)
+                .setTitle("Done parsing")
+                .setMessage(ThermostatData.current_day)
+                .show();
+    }
+
+
+
+    // Simple method to simplify converting the string returned in HTTP responses
+    // to InputStreams for use in XML parsing.
+    public static InputStream stringToInputStream(String str){
+        InputStream stream = new ByteArrayInputStream(str.getBytes());
+        return stream;
     }
 }
---
+++ app/src/main/java/com/group09/hti_thermostat/ThermostatData.java
@@ -0,0 +1,47 @@
+package com.group09.hti_thermostat;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+/**
+ * Created by nick on 01-Jun-16.
+ */
+public class ThermostatData {
+    public static String current_day;
+    public static String time;
+    public static String current_temperature;
+    public static String target_temperature;
+    public static String day_temperature;
+    public static String night_temperature;
+
+    // "on" or "off". Why they didn't just use a bool in the xml? Who fucking knows.
+    public static String week_program_state;
+
+    public static void readThermostatData(XmlPullParser parser, String type) throws IOException, XmlPullParserException{
+        String text = "";
+        parser.require(XmlPullParser.START_TAG, null, type);
+        if(parser.next() == XmlPullParser.TEXT){
+            text = parser.getText();
+            parser.nextTag();
+        }
+        parser.require(XmlPullParser.END_TAG, null, type);
+        switch(type){
+            case "current_day":
+                current_day = text;
+            case "time":
+                time = text;
+            case "current_temperature":
+                current_temperature = text;
+            case "target_temperature":
+                target_temperature = text;
+            case "day_temperature":
+                day_temperature = text;
+            case "night_temperature":
+                night_temperature = text;
+            case "week_program_state":
+                week_program_state = text;
+        }
+    }
+}
