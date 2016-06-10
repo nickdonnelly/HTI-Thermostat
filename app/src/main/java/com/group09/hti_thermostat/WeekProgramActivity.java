9ee0a372d2c0dc4f2d9b22942a803534b47b78cd
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-06-10 11:39:14 +0200

    Fixed deprecated code use.

--- app/src/main/java/com/group09/hti_thermostat/WeekProgramActivity.java
+++ app/src/main/java/com/group09/hti_thermostat/WeekProgramActivity.java
@@ -3,6 +3,7 @@ package com.group09.hti_thermostat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,7 +18,7 @@ public class WeekProgramActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_black_24dp));
+        fab.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_add_black_24dp, null));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
